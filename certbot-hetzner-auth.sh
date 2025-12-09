49f0ba4c0630e971c6860ec3369075f010111e7e
Maingron <r@maingron.com>
2025-12-09 15:07:11 +0100

    Add ttl variable and set ttl to 60

--- certbot-hetzner-auth.sh
+++ certbot-hetzner-auth.sh
@@ -3,6 +3,7 @@
 token=$(cat /etc/hetzner-dns-token)
 domain_name=$( echo $CERTBOT_DOMAIN | rev | cut -d'.' -f 1,2 | rev)
 rr_name=_acme-challenge.${CERTBOT_DOMAIN%$domain_name}
+ttl=60 # As of Dec 2025, 60 is the minimum TTL for Hetzner Zones
 rr_type=TXT
 # Remove dot for non wildcard requests
 rr_name=${rr_name%.}
@@ -10,7 +11,7 @@ rr_name=${rr_name%.}
 curl -X "POST" "https://api.hetzner.cloud/v1/zones/${domain_name}/rrsets" \
      -H "Content-Type: application/json" \
      -H "Authorization: Bearer ${token}" \
-     -d "{ \"name\":\"${rr_name}\", \"type\":\"${rr_type}\", \"ttl\":300, \"records\":[{\"value\":\"\\\"${CERTBOT_VALIDATION}\\\"\"}] }" > /dev/null 2>/dev/null
+     -d "{ \"name\":\"${rr_name}\", \"type\":\"${rr_type}\", \"ttl\":${ttl}, \"records\":[{\"value\":\"\\\"${CERTBOT_VALIDATION}\\\"\"}] }" > /dev/null 2>/dev/null
 
 # just make sure we sleep for a while (this should be a dig poll loop)
 sleep 30
